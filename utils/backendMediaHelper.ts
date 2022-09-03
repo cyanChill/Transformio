faba4f97f91f07112239f2aa6421d270b79469f2
cyanChill <anthonyliang9@gmail.com>
2022-09-03 15:45:40 -0400

    changed output format & codec to potentially reduce memory usage

--- utils/backendMediaHelper.ts
+++ utils/backendMediaHelper.ts
@@ -12,10 +12,8 @@ export const convertVideo = (fileObj: formidable.File) => {
     const chunks: Buffer[] = [];
 
     const cmd = ffmpeg(fileObj.filepath)
-      .toFormat("mp4")
-      // In order to output mp4 in stream: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/346#issuecomment-67299526
-      .outputOptions("-movflags frag_keyframe+empty_moov")
-      // Return the combined buffer of converted video
+      .videoCodec("libx265")
+      .toFormat("matroska") // Output as .mkv format
       .on("end", () => resolve(Buffer.concat(chunks)))
       .on("progress", (progress) => progressDebug(progress))
       .on("error", (err, stdout, stderr) => {
